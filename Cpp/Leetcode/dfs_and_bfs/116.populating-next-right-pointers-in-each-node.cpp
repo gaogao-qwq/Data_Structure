25e815df5861f725917be28b9141499efaceb8bd
gaogao-qwq <gaogao-qwq>
2022-11-09 22:19:07 +0800

    Update LC answers

--- Cpp/Leetcode/dfs_and_bfs/116.populating-next-right-pointers-in-each-node.cpp
+++ Cpp/Leetcode/dfs_and_bfs/116.populating-next-right-pointers-in-each-node.cpp
@@ -14,3 +14,43 @@ public:
     Node(int _val, Node* _left, Node* _right, Node* _next)
         : val(_val), left(_left), right(_right), next(_next) {}
 };
+
+class Solution {
+public:
+    inline Node* connect(Node* root) {
+        if (root == nullptr) return root;
+        deque<Node *> que;
+        // 根节点推入队列
+        que.push_back(root);
+        auto p = root;
+        while (!que.empty()) {
+            int size = que.size();
+            // 将队列中的首位存起来
+            auto curr = que.front();
+            // 将队列中的元素串联起来
+            for (int i = 1; i < size; ++i) {
+                curr->next = que[i];
+                curr = que[i];
+            }
+            // 遍历队列中的每个元素，将每个元素的左右节点也放入队列中
+            for (int i = 0; i < size; ++i) {
+                curr = que.front();
+                que.pop_front();
+                if (curr->left != nullptr) {
+                    que.push_back(curr->left);
+                }
+                if (curr->right != nullptr) {
+                    que.push_back(curr->right);
+                }
+            }
+        }
+        return root;
+    }
+};
+
+int main() {
+    Solution s;
+    auto root = new Node(1, new Node(2, new Node(4), new Node(5), nullptr), new Node(3, new Node(6), new Node(7), nullptr), nullptr);
+    root = s.connect(root);
+    return 0;
+}
