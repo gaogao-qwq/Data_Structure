22cb09ddb23fbc7327816fffdf5243aec0cf4ef0
gaogao-qwq <gaogao-qwq>
2022-11-15 11:58:29 +0800

    Update Luogu answers

--- Cpp/Luogu/dp/P1510.cpp
+++ Cpp/Luogu/dp/P1510.cpp
@@ -9,13 +9,15 @@ int main() {
         cin >> v[i] >> c[i];
     }
 
-    vector<int> dp(V + 1);
+    vector<int> dp(C + 1, 0);
+    int ans = -1;
     for (int i = 0; i < n; ++i) {
-        for (int j = V; j >= v[i]; --j) {
-            dp[j] = max(dp[j], dp[j - v[i]] + c[i]);
+        for (int j = C; j >= c[i]; --j) {
+            dp[j] = max(dp[j], dp[j - c[i]] + v[i]);
+            if (dp[j] >= V) ans = max(ans, C - j);
         }
     }
-    if (C - dp[V] < 0) cout << "Impossible";
-    else cout << C - dp[V];
+    if (ans == -1) cout << "Impossible";
+    else cout << ans;
     return 0;
 }
