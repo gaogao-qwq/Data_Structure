412fd02ed073720049138faa5fce23fa95914b98
gaogao-qwq <gaogao-qwq>
2022-11-16 20:43:57 +0800

    Update Luogu answers

--- Cpp/Luogu/dp/P1164.cpp
+++ Cpp/Luogu/dp/P1164.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 01背包改编
 int main() {
     int N = 0, M = 0;
     cin >> N >> M;
@@ -10,14 +11,16 @@ int main() {
     }
 
     vector<int> dp(M + 1, 0);
+    dp[0] = 1;
     for (int i = 0; i < N; ++i) {
         for (int j = M; j >= v[i]; --j) {
-            dp[j] = max(dp[j], dp[j - v[i]] + v[i]);
+            dp[j] += dp[j - v[i]];
         }
-        for (auto i : dp) {
-            cout << i << " ";
-        }
-        cout << endl;
+        // for (auto i : dp) {
+        //     cout << i << " ";
+        // }
+        // cout << endl;
     }
+    cout << dp[M];
     return 0;
 }
