8b1855061c3c1a502c6359e2918127b832489c9f
gaogao_qwq <gaogaoqwq@gmail.com>
2022-11-22 01:50:27 +0000

    Update Luogu answers

---
+++ Cpp/Luogu/queue/P1160.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    deque<int> deq;
+    deq.push_front(1);
+    int n = 0;
+    cin >> n;
+    for (int i = 1; i < n; ++i) 
+    return 0;
+}
